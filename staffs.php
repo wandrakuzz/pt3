f265c8c877ca049d9fd7a00d99345e9ccaa5d4e4
wandrakuzz <wanpsis92@gmail.com>
2016-05-22 00:58:46 +0800

    done with staff

--- staffs.php
+++ staffs.php
@@ -17,6 +17,7 @@
 </head>
 <body>
   <?php include_once 'nav_bar.php'; ?>
+
   <div class="container-fluid">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -52,81 +53,80 @@
           </div>
           </div>
         </div>
-
-
-
-
-
+        <div class="form-group">
+            <label for="staffphone" class="col-sm-3 control-label">Phone No.</label>
+              <div class="col-sm-7">
+                <input name="phone" type="text" class="form-control" placeholder="Customer Phone Number" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_customer_phone_num']; ?>" required>
+              </div>
+        </div>
+        <div class="form-group">
+            <label for="staffemail" class="col-sm-3 control-label">Email</label>
+              <div class="col-sm-7">
+                <input name="email" type="text" class="form-control" placeholder="Customer Email" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_customer_email']; ?>" required>
+              </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-3 col-sm-9">
+        <?php if (isset($_GET['edit'])) { ?>
+        <input type="hidden" name="oldsid" value="<?php echo $editrow['fld_staff_ID']; ?>">
+        <button class="btn btn-primary" type="submit" name="update"><span class="glyphicon glyphicon-pencil" aria-hidden="true"> Update</button>
+        <?php } else { ?>
+        <button class="btn btn-success" type="submit" name="create"><span class="glyphicon glyphicon-plus" aria-hidden="true"> Create</button>
+        <?php } ?>
+        <button class="btn btn-danger" type="reset"><span class="glyphicon glyphicon-erase" aria-hidden="true"> Clear</button>
+        </div>
       </div>
+      </form>
+    </div>
+    </div>
+
+<div class="row">
+  <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+    <div class="page-header">
+      <h2>Staff Lists</h2>
+
+      <table class="table table-striped table-bordered">
+        <tr>
+          <td>Staff ID</td>
+          <td>Name</td>
+          <td>Gender</td>
+          <td>Phone Number</td>
+          <td>Email Address</td>
+          <td></td>
+        </tr>
+        <?php
+        // Read
+        try {
+          $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT * FROM tbl_staffs_a148647");
+          $stmt->execute();
+          $result = $stmt->fetchAll();
+        }
+        catch(PDOException $e){
+              echo "Error: " . $e->getMessage();
+        }
+        foreach($result as $readrow) {
+        ?>
+        <tr>
+          <td><?php echo $readrow['fld_staff_ID']; ?></td>
+          <td><?php echo $readrow['fld_staff_name']; ?></td>
+          <td><?php echo $readrow['fld_staff_gender']; ?></td>
+          <td><?php echo $readrow['fld_staff_phone_num']; ?></td>
+          <td><?php echo $readrow['fld_staff_email']; ?></td>
+          <td>
+            <a href="staffs.php?edit=<?php echo $readrow['fld_staff_ID']; ?>" class="btn btn-success btn-xs" role="button">Edit</a>
+            <a href="staffs.php?delete=<?php echo $readrow['fld_staff_ID']; ?>" onclick="return confirm('Are you sure to delete?');" class="btn btn-danger btn-xs" role="button">Delete</a>
+          </td>
+        </tr>
+        <?php
+        }
+        $conn = null;
+        ?>
+      </table>
     </div>
   </div>
-
-
-
-
-
-
-
-    <hr>
-    <form action="staffs.php" method="post">
-      Customer ID
-      <input name="sid" type="text" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_staff_ID']; ?>"> <br>
-      Name
-      <input name="name" type="text" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_staff_name']; ?>"> <br>
-      Gender
-      <input name="gender" type="radio" value="Male" <?php if(isset($_GET['edit'])) if($editrow['fld_staff_gender']=="Male") echo "checked"; ?>> Male
-      <input name="gender" type="radio" value="Female" <?php if(isset($_GET['edit'])) if($editrow['fld_staff_gender']=="Female") echo "checked"; ?>> Female <br>
-      Phone Number
-      <input name="phone" type="text" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_staff_phone_num']; ?>"> <br>
-      Email Address
-      <input name="email" type="text" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_staff_email']; ?>"> <br>
-      <?php if (isset($_GET['edit'])) { ?>
-      <input type="hidden" name="oldsid" value="<?php echo $editrow['fld_staff_ID']; ?>">
-      <button type="submit" name="update">Update</button>
-      <?php } else { ?>
-      <button type="submit" name="create">Create</button>
-      <?php } ?>
-      <button type="reset">Clear</button>
-    </form>
-    <hr>
-    <table border="1">
-      <tr>
-        <td>Staff ID</td>
-        <td>Name</td>
-        <td>Gender</td>
-        <td>Phone Number</td>
-        <td>Email Address</td>
-        <td></td>
-      </tr>
-      <?php
-      // Read
-      try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          $stmt = $conn->prepare("SELECT * FROM tbl_staffs_a148647");
-        $stmt->execute();
-        $result = $stmt->fetchAll();
-      }
-      catch(PDOException $e){
-            echo "Error: " . $e->getMessage();
-      }
-      foreach($result as $readrow) {
-      ?>
-      <tr>
-        <td><?php echo $readrow['fld_staff_ID']; ?></td>
-        <td><?php echo $readrow['fld_staff_name']; ?></td>
-        <td><?php echo $readrow['fld_staff_gender']; ?></td>
-        <td><?php echo $readrow['fld_staff_phone_num']; ?></td>
-        <td><?php echo $readrow['fld_staff_email']; ?></td>
-        <td>
-          <a href="staffs.php?edit=<?php echo $readrow['fld_staff_ID']; ?>">Edit</a>
-          <a href="staffs.php?delete=<?php echo $readrow['fld_staff_ID']; ?>" onclick="return confirm('Are you sure to delete?');">Delete</a>
-        </td>
-      </tr>
-      <?php
-      }
-      $conn = null;
-      ?>
-    </table>
+</div>
+</div>
 </body>
 </html>
